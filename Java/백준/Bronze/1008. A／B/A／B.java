b76e06c11aa6832f45af8db9188851ce0891b315
spatially021 <82856043+spatially021@users.noreply.github.com>
2024-10-13 20:27:36 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17996 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        double a=scan.nextDouble();
+        double b=scan.nextDouble();
+        System.out.println(a/b);
+    }
+}
