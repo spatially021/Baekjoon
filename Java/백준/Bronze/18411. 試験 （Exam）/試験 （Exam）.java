1f5fab7bfd68f3502b3a5eb26f2077bee9492a7c
spatially021 <82856043+spatially021@users.noreply.github.com>
2024-10-21 20:34:04 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ Java/백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st=new StringTokenizer(br.readLine()," ");
+
+        int[] arr=new int[3];
+
+        for(int i=0;i<3;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[1]+arr[2]);
+
+    }
+
+}
