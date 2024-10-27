f243ff859a3d4f194a2c878e1c10945bc19031c3
spatially021 <82856043+spatially021@users.noreply.github.com>
2024-10-27 19:38:42 +0900

    [Silver V] Title: 배열 합치기, Time: 1344 ms, Memory: 301948 KB -BaekjoonHub

--- Java/백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ Java/백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -8,27 +8,43 @@ public class Main {
 
         st = new StringTokenizer(br.readLine(), " ");
 
-        int n=Integer.parseInt(st.nextToken());
-        int m=Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
 
-        st=new StringTokenizer(br.readLine()," ");
+        st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i=0;i<n;i++){
-            pq.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
 
-        st=new StringTokenizer(br.readLine()," ");
+        st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i=0;i<m;i++){
-            pq.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < m; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
-        StringBuilder sb=new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-        while (!pq.isEmpty()){
-            sb.append(pq.poll()).append(" ");
+        int pointer1 = 0;
+        int pointer2 = 0;
+
+        while (pointer1 < n || pointer2 < m) {
+
+            if (pointer1 >= n) {
+                sb.append(arr2[pointer2++]).append(" ");
+                continue;
+            }
+            if (pointer2 >= m) {
+                sb.append(arr1[pointer1++]).append(" ");
+                continue;
+            }
+
+            if (arr1[pointer1] < arr2[pointer2]) sb.append(arr1[pointer1++]).append(" ");
+            else sb.append(arr2[pointer2++]).append(" ");
+
         }
 
         System.out.println(sb);
