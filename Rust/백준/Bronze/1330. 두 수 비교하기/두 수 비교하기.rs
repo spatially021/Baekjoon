e5c0b547535d8e74212e648cf771839a593de566
spatially021 <82856043+spatially021@users.noreply.github.com>
2024-12-05 13:56:21 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ Rust/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+
+    let mut input=String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let mut numbers=input.split_whitespace();
+    let a:i32=numbers.next().unwrap().parse().unwrap();
+    let b:i32=numbers.next().unwrap().parse().unwrap();
+    
+    if a==b {
+        println!("==");
+    }else if a>b{
+        println!(">");
+    }else {
+        println!("<");
+    }
+    
+}
