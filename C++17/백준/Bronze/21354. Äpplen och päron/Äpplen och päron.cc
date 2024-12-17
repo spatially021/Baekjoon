7092669ac3dfcbfabd2dc4693b8eed5400f4b5db
spatially021 <82856043+spatially021@users.noreply.github.com>
2024-12-17 20:42:17 +0900

    [Bronze IV] Title: Äpplen och päron, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/21354. Äpplen och päron/Äpplen och päron.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    
+    cin>>a>>b;
+    
+    a*=7;
+    b*=13;
+    
+    if(a==b) cout<<"lika";
+    else cout<<(a>b?"Axel":"Petra");
+    
+    
+}
