a7727e73882cc8ce6186e1759083b820d0a1cd91
spatially021 <82856043+spatially021@users.noreply.github.com>
2025-01-06 16:05:27 +0900

    [Gold V] Title: 월요일-토요일, Time: 1352 ms, Memory: 22648 KB -BaekjoonHub

--- Java/백준/Gold/4969. 월요일－토요일/월요일－토요일.java
+++ Java/백준/Gold/4969. 월요일－토요일/월요일－토요일.java
@@ -6,35 +6,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BitSet B = new BitSet(300_001);
         ArrayList<Integer> monSat = new ArrayList<>();
+        boolean[] B = new boolean[300_001];
 
         for (int i = 8; i < 300_001; i += 7) {
             monSat.add(i);
-            B.set(i);
+            B[i] = true;
         }
         for (int i = 6; i < 300_001; i += 7) {
             monSat.add(i);
-            B.set(i);
+            B[i] = true;
         }
+
+        Collections.sort(monSat);
+
+        boolean[] prim = new boolean[300_001];
 
         while (true) {
             int num = Integer.parseInt(br.readLine());
             if (num == 1) break;
 
             bw.write(num + ": ");
-
-            BitSet prim = new BitSet(300_001);
-            prim.or(B);
+            System.arraycopy(B, 0, prim, 0, 300_001); // 기존 배열을 복사하지 않고 메모리 효율적으로 초기화
 
             for (int i : monSat) {
                 if (num % i == 0) {
-                    for (int j = i * 2; j < 300_001; j += i) prim.clear(j);
-                } else prim.clear(i);
+                    for (int j = i * 2; j < 300_001; j += i) prim[j] = false;
+                } else prim[i] = false;
             }
 
-            for (int i : prim.stream().toArray()) bw.write(i + " ");
-
+            for (int i : monSat) if (prim[i]) bw.write(i + " ");
             bw.write("\n");
         }
 
