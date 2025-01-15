064e041643a8039707fe8ef7ccfed26a8340701c
spatially021 <82856043+spatially021@users.noreply.github.com>
2025-01-15 13:05:03 +0900

    [Gold V] Title: 연산자 파티, Time: 64 ms, Memory: 13208 KB, Score: 100 point -BaekjoonHub

--- Rust/백준/Gold/27738. 연산자 파티/연산자 파티.rs
+++ Rust/백준/Gold/27738. 연산자 파티/연산자 파티.rs
@@ -4,36 +4,36 @@ fn main() {
     let mut buffer = String::new();
 
     io::stdin().read_line(&mut buffer).unwrap();
-    let n: i128 = buffer.trim().parse::<i128>().unwrap();
+    let n: u128 = buffer.trim().parse::<u128>().unwrap();
 
     buffer.clear();
 
     io::stdin().read_line(&mut buffer).unwrap();
-    let vec: Vec<i128> = buffer
+    let vec: Vec<u128> = buffer
         .split_whitespace()
         .map(|x| x.parse().unwrap())
         .collect();
 
-    let mut x: u128 = 0; // x의 타입을 u128로 변경
+    let mut x: u128 = 0;
 
-    for i in 1..=n {
+    for i in n/vec[5]*vec[5]+1..=n {
         if i % vec[0] == 0 {
-            x += i as u128;
+            x += i;
         }
         if i % vec[1] == 0 {
-            x %= i as u128;
+            x %= i;
         }
         if i % vec[2] == 0 {
-            x &= i as u128;
+            x &= i;
         }
         if i % vec[3] == 0 {
-            x ^= i as u128;
+            x ^= i;
         }
         if i % vec[4] == 0 {
-            x |= i as u128;
+            x |= i;
         }
         if i % vec[5] == 0 {
-            x =x >>(i as u128);
+            x >>= i;   
         }
     }
 
