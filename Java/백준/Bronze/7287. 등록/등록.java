268326fcfac701a340ab82656325e7552021e3e3
spatially021 <82856043+spatially021@users.noreply.github.com>
2025-02-07 09:05:54 +0900

    [Bronze V] Title: 등록, Time: 196 ms, Memory: 16964 KB -BaekjoonHub

---
+++ Java/백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main { 
+
+    public static void main(String[] args)  {
+
+        Scanner scan=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder();
+
+        System.out.println(170+"\nspatially");
+
+        scan.close();
+
+    }
+}
