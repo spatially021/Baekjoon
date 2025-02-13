be1535cbe0a28939edd8d3962c7d27ae41da1499
spatially021 <82856043+spatially021@users.noreply.github.com>
2025-02-13 14:19:19 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ Rust/백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.rs
@@ -0,0 +1,9 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+
+    print!("{}", input.next().unwrap() / 5);
+}
