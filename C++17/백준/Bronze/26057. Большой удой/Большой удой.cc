de5e699e96db62b2f142945935db8023a547f4d0
spatially021 <82856043+spatially021@users.noreply.github.com>
2025-03-16 20:13:51 +0900

    [Bronze IV] Title: Большой удой, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26057. Большой удой/Большой удой.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int l, t; cin >> l >> t;
+
+  int diff = 2 * t - l;
+  cout << diff << "\n";
+
+  return 0;
+}
