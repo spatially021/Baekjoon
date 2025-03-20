167d75f80bb816a5c57475e649e8321ac0ad61b7
spatially021 <82856043+spatially021@users.noreply.github.com>
2025-03-21 00:15:26 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<(a*1000+b*10000);
+    
+    return 0;
+}
